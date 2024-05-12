a984e980062a8c9eed724d9d2dd28c336e2de22d
deivid94 <md031194@gmail.com>
2024-05-11 23:52:17 -0300

    Feat criado Classe menu para solicitar uma serie pelo usario; Refartoring: desacoplando as classes da Classe menu

--- demo/src/main/java/com/example/demo/AluraSpringBootApplication.java
+++ demo/src/main/java/com/example/demo/AluraSpringBootApplication.java
@@ -1,16 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.model.DadosEpsodios;
-import com.example.demo.model.DadosSeries;
-import com.example.demo.model.DadosTemporada;
+
+import com.example.demo.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import service.ConsumoApi;
-import service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 @SpringBootApplication
 public class AluraSpringBootApplication  implements CommandLineRunner {
@@ -21,29 +18,24 @@ public class AluraSpringBootApplication  implements CommandLineRunner {
 
     @Override                   //metodoMain
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoApi();
+        Principal menu = new Principal();
+        menu.exibeMenu();
 
-
-        var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=6de054f5");
-        ConverteDados conversor = new ConverteDados();
-        DadosSeries dadoSerie = conversor.obterDados(json, DadosSeries.class);
-        //System.out.println(dadoSerie);
-
-
-        json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&&season=1&episode=2&apikey=6de054f5");
-        DadosEpsodios dadosEpsodios = conversor.obterDados(json, DadosEpsodios.class);
-       // System.out.println(dadosEpsodios);
-
-        json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&&season=1&apikey=6de054f5");
-         DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-        System.out.println(dadosTemporada);
+//
+//        List<DadosTemporada> temporadas = new ArrayList<>();
+//         for (int i = 1 ; i<= dadoSerie.totaLTemporadas(); i ++){
+//             json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&&season="+i+"&apikey=6de054f5");
+//             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//             temporadas.add(dadosTemporada);
+//             temporadas.forEach(System.out::println);
+         }
 
 
 
 
 
 
-        }
+
 
 
     }
--- demo/src/main/java/com/example/demo/principal/Principal.java
+++ demo/src/main/java/com/example/demo/principal/Principal.java
@@ -1,22 +1,26 @@
 package com.example.demo.principal;
-
+import com.example.demo.model.DadosSeries;
 import service.ConsumoApi;
+import service.ConverteDados;
 
 import java.util.Scanner;
 
 
 
 public class Principal {
-    private final String APIKEY = "http://www.omdbapi.com/?t=";
-    private final String ENDERECO = "&apikey=6de054f5";
+    private final String ENDERECO = "http://www.omdbapi.com/?t=";
+    private final String APIKEY = "&apikey=6de054f5";
     private Scanner leitura = new Scanner(System.in);
+    private  ConsumoApi consumoAPi = new ConsumoApi();
+    private ConverteDados converteDados = new ConverteDados();
+
 
 
         public void exibeMenu(){
             System.out.println("Digite o nome para serie para busca: ");
-
             var nomeSerie = leitura.nextLine();
-            ConsumoApi consumoApi = new ConsumoApi();
-            var json = consumoApi.obterDados(ENDERECO+nomeSerie.replace(" ", "+")+APIKEY);
+            var json = consumoAPi.obterDados(ENDERECO+nomeSerie.replace(" ", "+")+APIKEY);
+            DadosSeries dadoSerie = converteDados.obterDados(json, DadosSeries.class);
+            System.out.println(dadoSerie);
         }
 }
